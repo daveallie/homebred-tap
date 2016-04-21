7fb3ebddac2188481940f62878b56d0f6e872bfa
Dave Allie <dave@tanda.co>
2016-04-21 13:44:39 +1000

    Update git-tools to 1.0.3

--- git-tools.rb
+++ git-tools.rb
@@ -1,12 +1,8 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class GitTools < Formula
   homepage "https://github.com/daveallie/git-tools"
-  url "https://github.com/daveallie/git-tools/archive/1.0.2.tar.gz"
-  version "1.0.2"
-  sha256 "95c087042a5b84529f8f8b380abdc69d2b74d9a4e3ebffd24295ac14326583e3"
+  url "https://github.com/daveallie/git-tools/archive/1.0.3.tar.gz"
+  version "1.0.3"
+  sha256 "1710989fc4b3e89878040861e00ce084060355efb81b4637c39a1766be840201"
 
   def install
     bin.install 'git-tools'
