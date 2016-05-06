a4531d572cb42093979dcc1ffb0626b0c2ea77d6
Dave Allie <dave@daveallie.com>
2016-05-06 13:57:29 +1000

    Update git-tools to 1.0.4

--- git-tools.rb
+++ git-tools.rb
@@ -1,8 +1,8 @@
 class GitTools < Formula
   homepage "https://github.com/daveallie/git-tools"
-  url "https://github.com/daveallie/git-tools/archive/1.0.3.tar.gz"
-  version "1.0.3"
-  sha256 "1710989fc4b3e89878040861e00ce084060355efb81b4637c39a1766be840201"
+  url "https://github.com/daveallie/git-tools/archive/1.0.4.tar.gz"
+  version "1.0.4"
+  sha256 "a6c34c05c94d1015db4de5e665bc6f0a11c5e3417f6a6c6cee0cf839fa607121"
 
   def install
     bin.install 'git-tools'
