af7516f3d8be3d7ff1f9b6c3d04fa604bafdbb63
Dave Allie <dave@daveallie.com>
2016-05-09 18:11:39 +1000

    Version not needed, update desc

--- git-tools.rb
+++ git-tools.rb
@@ -1,8 +1,7 @@
 class GitTools < Formula
-  desc "A small OS X utility tool for managing branches in Git repositories."
+  desc "Small OS X utility tool for managing branches in Git repositories."
   homepage "https://github.com/daveallie/git-tools"
-  version "1.2.0"
-  url "https://github.com/daveallie/git-tools/archive/#{version}.tar.gz"
+  url "https://github.com/daveallie/git-tools/archive/1.2.0.tar.gz"
   sha256 "d399c43f1bc88e21db6a4f7124c8ca0819340a20b9090b751bc996c5f499d32e"
 
   def install
