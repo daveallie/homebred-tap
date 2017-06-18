0079f37be31798d070bb7e00b836a726af6e64a6
Dave Allie <dave@daveallie.com>
2017-06-18 18:04:18 +1000

    Update bindrs to 0.1.1

--- bindrs.rb
+++ bindrs.rb
@@ -1,8 +1,8 @@
 class Bindrs < Formula
   desc "Two way file syncer using platform native notify."
   homepage "https://github.com/daveallie/bindrs"
-  url "https://github.com/daveallie/bindrs/releases/download/v0.1.0/bindrs-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "8f9a7aae2db79925bb143684b011be5c16bdc26f05932b51edf7b76133e0150c"
+  url "https://github.com/daveallie/bindrs/releases/download/v0.1.1/bindrs-0.1.1-x86_64-apple-darwin.tar.gz"
+  sha256 "b5395083c9455321c4209109d66fc65a462b58a7e3f8c670cc40d22394835cc5"
 
   def install
     bin.install "bindrs"
